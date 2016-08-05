9577842b4ecef9c26a0c459813757ecc94cae470
Yasuhiro Asaka <yasuhiro.asaka@grauwoelfchen.net>
2016-08-05 17:31:51 +0200

    Remove REST version

--- main.go
+++ main.go
@@ -1,15 +1,6 @@
 package main
 
 import (
-	//"encoding/json"
-	//"fmt"
-	//"io/ioutil"
-	//"log"
-	//"os"
-
-	//"golang.org/x/oauth2"
-	//"golang.org/x/oauth2/google"
-
 	"fmt"
 	"log"
 	"os"
@@ -20,74 +11,18 @@ import (
 	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/credentials/oauth"
 
-	pb "github.com/grauwoelfchen/untertiteln/proto"
+	pb "github.com/grauwoelfchen/untertiteln/proto/sync_recognize"
 )
 
-const scope string = "https://www.googleapis.com/auth/cloud-platform"
-
-// rest
-const url string = "https://speech.googleapis.com"
-const path string = "/v1beta1/speech:syncrecognize"
-
-// grpc
-const address string = "speech.googleapis.com:443"
-const method string = "/google.cloud.speech.v1beta1.Speech/SyncRecognize"
-
-// Alternative is object holds transcribed text
-type Alternative struct {
-	Transcript string
-	Confidence float64
-}
-
-// Result contains Alternative entries
-type Result struct {
-	Alternatives []Alternative
-}
-
-// Response contains results
-type Response struct {
-	Results []Result `json:results`
-}
+const (
+  scope string = "https://www.googleapis.com/auth/cloud-platform"
+  address string = "speech.googleapis.com:443"
+  method string = "/google.cloud.speech.v1beta1.Speech/SyncRecognize"
+)
 
 func main() {
 	keyFile := os.Getenv("GOOGLE_APPLICATION_CREDENTIALS")
 
-	// REST syncrecognize
-	//keyData, err := ioutil.ReadFile(keyFile)
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-
-	//conf, err := google.JWTConfigFromJSON(keyData, scope)
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-	//client := conf.Client(oauth2.NoContext)
-
-	//buf, err := os.Open("./sync-request.json")
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-
-	//res, err := client.Post(url+path, "application/json", buf)
-	//defer res.Body.Close()
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-	//if res.StatusCode != 200 {
-	//	log.Fatal(res.Header)
-	//	return
-	//}
-
-	//decoder := json.NewDecoder(res.Body)
-	//var response Response
-	//err = decoder.Decode(&response)
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-	//fmt.Println(response.Results[0].Alternatives[0].Transcript)
-
-	// gRPC SyncRecognize
 	conf, err := oauth.NewServiceAccountFromFile(keyFile, scope)
 	if err != nil {
 		log.Fatal(err)
